8a73ff9bfe9a058980592c7c07cc54daf96c3c0d
YuliiaBondar <Yulia.B@adopy.com>
2023-12-18 17:27:46 +0200

    Add main page + cleanup

--- main_page/js/18149.43c7686b768237e670b0.js
+++
@@ -1,2 +0,0 @@
-(self.__LOADABLE_LOADED_CHUNKS__=self.__LOADABLE_LOADED_CHUNKS__||[]).push([[18149],{18149:(_,A,D)=>{var L=D(88746);_.exports=function(_,A){return L(_,A)}}}]);
-//# sourceMappingURL=18149.js.map
